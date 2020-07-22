c001a43f4cd09896431a70f48656daf83d1956b3
Getraline <getraline@getraline-VB-Ubuntu18.04>
2020-07-22 09:59:22 +0200

    add component Users and hard code array users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import Navbar from './components/layout/Navbar'
-import UserItem from './components/users/UserItem'
+import Users from './components/users/Users'
 import './App.css'
 
 class App extends Component {
@@ -14,7 +14,9 @@ class App extends Component {
 			// </Fragment>
 			<div className='App'>
 				<Navbar />
-				<UserItem />
+				<div className="container">
+					<Users />
+				</div>
 			</div>
 		);
 
